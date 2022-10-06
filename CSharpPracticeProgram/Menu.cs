bff1f8b8f084f29456c093669564827cb402b0c4
Amy Tomey <amy.tomey59@@gmail.com>
2022-10-06 12:53:47 -0400

    part 2 complete... missing display employees. deleted 1 duplicate file

--- CSharpPracticeProgram/Menu.cs
+++ CSharpPracticeProgram/Menu.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -9,54 +10,71 @@ namespace CSharpPracticeProgram
 {
     internal class Menu
     {
-        public List<Employee> AddEmployee(List<Employee> employeeList)
+        public static List<Employee> AddEmployee(List<Employee> employeeList)
+        {
+            Console.WriteLine("Please enter your name:");
+            var name = Console.ReadLine();
+
+                Console.WriteLine("Please enter your birthday:");
+            var birthdayInput = Console.ReadLine();
+
+            DateTime birthday;
+
+            while (!DateTime.TryParse(birthdayInput, out birthday))
             {
-                Console.WriteLine("Please enter your name:");
-                var name = Console.ReadLine();
-
-                 Console.WriteLine("Please enter your birthday:");
-                var birthdayInput = Console.ReadLine();
-
-                DateTime birthday;
-
-                while (!DateTime.TryParse(birthdayInput, out birthday))
-{
                 Console.WriteLine("Not a valid date, please try again:");
                  birthdayInput = Console.ReadLine();
- }
+            }
 
-                Console.WriteLine("Please enter your salary:");
-                var salary = Console.ReadLine();
+            Console.WriteLine("Please enter your salary:");
+            var salary = Console.ReadLine();
 
-                var employee = new Employee
+            var employee = new Employee
             {
                 Name = name,
                 Birthday = birthday,
                 Salary = decimal.Parse(salary)
             };
 
-            Console.WriteLine($"Welcome to the company, {employee.Name}! Your salary is {employee.Salary} and your birthday is {employee.Birthday.ToShortDateString()}.");
+            employeeList.Add(employee);
 
-            DateTime myNextBirthday = new(DateTime.Today.Year, employee.Birthday.Month, employee.Birthday.Day);
+            Console.WriteLine($"Welcome to the company, {employee.Name}! " +
+                $"Your salary is {employee.Salary} " +
+                $"and your birthday is {employee.Birthday.ToShortDateString()}.");
 
-            if (myNextBirthday > DateTime.Today)
-            {
-                myNextBirthday = myNextBirthday.AddYears(1);
-            }
-
-            var daysUntilBirtday = (myNextBirthday - DateTime.Today).Days;
-
-            if (daysUntilBirtday == 0)
-            {
-                Console.WriteLine("HAPPY BIRTHDAY!!!!");
-            }
-            else
-            {
-                Console.WriteLine($"Your birthday will be in {daysUntilBirtday} days.");
-            }
+            var daysTillNext = GetDaysUntilBirthday(employee.Birthday);
+            Console.WriteLine($"It is {daysTillNext} days until the next birthday");
 
             return employeeList;
+        }
 
+        public static void DisplayEmployees1(List<Employee> employeeList)
+        {
+            foreach (Employee employee in employeeList)
+            {
+                var daysUntilBirthday = GetDaysUntilBirthday(employee.Birthday);
+                Console.WriteLine($"Employee {employee.Name} makes {employee.Salary} " +
+                     $"and was born on {employee.Birthday.ToShortDateString()}. It is " +
+                     $"{daysUntilBirthday} days until their next birthday");
+            }
+        }
+
+        public static List<Employee> RemoveEmployee(List<Employee> employeeList, string name)
+        {
+            var employeeToRemove = employeeList.First(item => item.Name == name);
+            employeeList.Remove(employeeToRemove);
+            return employeeList;
+        }
+
+        private static int GetDaysUntilBirthday(DateTime birthday)
+        {
+            DateTime nextBirthday = new DateTime(DateTime.Today.Year, birthday.Month, birthday.Day);
+            if (nextBirthday < DateTime.Today) //birthday already happened this year
+            {
+                nextBirthday = birthday.AddYears(1);
+            }
+
+            return (nextBirthday - DateTime.Today).Days;
         }
     }
 }
--- CSharpPracticeProgram/Program.cs
+++ CSharpPracticeProgram/Program.cs
@@ -6,13 +6,36 @@ using CSharpPracticeProgram;
 using System.Linq.Expressions;
 
 Console.WriteLine("Welcome to the program. Please choose from the following menu options:");
-Console.WriteLine("1. Add an employee");
-Console.WriteLine("2. View all current employees");
-Console.WriteLine("3. Remove an employee by name");
+Console.WriteLine("1: Add an employee");
+Console.WriteLine("2: View all current employees");
+Console.WriteLine("3: Remove an employee by name");
+Console.WriteLine("4: Exit");
 
-List<Employee> employees = new List<Employee>();
+List<Employee> employees = new();
 
-if (Console.ReadLine() == "1")
+string userInput = Console.ReadLine();
+
+while  (userInput != "4")
 {
-    employees = Menu.AddEmployee(employees);
+    if (userInput == "1")
+    {
+        employees = Menu.AddEmployee(employees);
+    }
+    else if (userInput == "2")
+    {
+        Menu.DisplayEmployees(employees);
+    }
+    else if (userInput == "3")
+    {
+        Console.WriteLine("Enter the name of the employee to remove");
+        var name = Console.ReadLine();
+        Menu.RemoveEmployee(employees, name);
+    }
+    else if (userInput == "4")// Exit
+    {
+       
+    }
+    Console.WriteLine("What next?");
+    userInput = Console.ReadLine();
+
 }
